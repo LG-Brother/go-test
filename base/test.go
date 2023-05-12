54eb26d04076497b420bfe53066ce026e576ef80
lingang <qq1812159202@163.com>
2023-05-12 16:47:33 +0800

    结构体语法使用

---
+++ base/test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var title = "test go"
+
+func getAge() int {
+	return 25
+}
+
+func getName() string {
+	return "LinGang"
+}
+
+func routine1() {
+	println("this is routine 1")
+	printRoutineInfo()
+}
+
+func routine2() {
+	println("this is routine 2")
+	printRoutineInfo()
+}
+
+func printRoutineInfo() {
+	buf := make([]byte, 2048)
+	stackInfo := runtime.Stack(buf, true)
+	fmt.Printf("====Begin stack tracke =====\n%v\n====End stack tracke====\n", stackInfo)
+}
+
+func main() {
+	printRoutineInfo()
+	name := getAge()
+	age := getName()
+	go routine1()
+	go routine2()
+	fmt.Println(title, name, age)
+	time.Sleep(10000)
+}
