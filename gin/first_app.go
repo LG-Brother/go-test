0f2167c94e4dbf2299f41ff5765faae58fcd1860
lingang <qq1812159202@163.com>
2023-05-19 20:03:38 +0800

    用gin创建一个服务

---
+++ gin/first_app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(context *gin.Context) {
+		context.String(200, "Hello, Gin")
+	})
+	r.Run(":8081")
+}
