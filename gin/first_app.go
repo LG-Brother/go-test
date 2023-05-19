5e64a79bc865677cf76fd66bcf59533a15b258cb
lingang <qq1812159202@163.com>
2023-05-19 20:15:09 +0800

    创建多个接口

--- gin/first_app.go
+++ gin/first_app.go
@@ -4,10 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func helloHandler(context *gin.Context) {
+	context.String(200, "This is my first Gin application")
+}
 func main() {
 	r := gin.Default()
 	r.GET("/", func(context *gin.Context) {
 		context.String(200, "Hello, Gin")
+	})
+	r.GET("/hello", helloHandler)
+	r.GET("/error", func(context *gin.Context) {
+		context.String(500, "This is the error page")
 	})
 	r.Run(":8081")
 }
